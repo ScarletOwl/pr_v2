c2637ea96193d6613b7c284866faca794d83ee5d
atnal <Atnalov@gmail.com>
2020-10-24 00:22:42 +0500

    Добавлен виджет NewsBox

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'news_box.dart';
+
 void main() => runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -7,6 +9,17 @@ void main() => runApp(
           appBar: AppBar(
             title: Text('RIDL'),
           ),
+          body: Center(
+            child: Column(
+              children: [
+                NewsBox('Супер сикретный чат',
+                    'Опасно для психологического здоровья лучше не входить',
+                    imageurl:
+                    'https://4.0-chan.ru/assets/images/src/2dd05b19c3e891f900040648477fa49c4d0f1f4a.png'),
+              ],
+            ),
+          ),
+
         ),
       ),
     );
